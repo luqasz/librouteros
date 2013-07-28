69dafa3bdffdd8ada3ac751ab81e2c0109c2be44
Łukasz Kostka <ukasz83@gmail.com>
2013-07-29 01:20:08 +0200

    removed tag properties from sentences objects tags can not be added to sentences self._words is list() not set()

--- rosapi/sentences.py
+++ rosapi/sentences.py
@@ -1,62 +1,42 @@
 # -*- coding: UTF-8 -*-
 
-from words import genericWord, ApiAttrWord
+from words import ApiAttrWord
+from exc import ApiError
 
-class genericSentence:
+
+class readableSentence:
     '''
-    A generic class that represents a sentence.
+    Class that represents read sentence.
     '''
 
 
-    def _set_tag( self, value ):
-        pass
-
-
-    def _get_tag( self ):
-        pass
-
-
-    tag = property( _get_tag, _set_tag )
-
-
-    def __init__( self ):
+    def __init__( self, words = [] ):
         '''
         Each element in _words is a word object.
         '''
-        self._words = []
 
-
-    def __isub__( self, word ):
-        '''
-        Method to remove word object from sentence.
-        sentence -= word
-        
-        This method may raise ValueError if word object is not present in the sentence
-        '''
-        self._words.remove( word )
-        return self
+        self._words = list( words )
 
 
     def __iadd__( self, word ):
         '''
-        Method to remove word object from sentence.
+        Method to add word object to sentence.
         sentence += word
         '''
+
+        if self.tag and isinstance( word, ApiAttrWord ):
+            raise ApiError( 'can not add more than one tag to sentence' )
+
         self._words.append( word )
         return self
 
 
-    def __contains__( self, word ):
+    def __iter__( self ):
         '''
-        Method to test word membership. Word may be an object, or string. 
-        If string then exact words as strings must match.
-        
-        :param word: word object or string
+        Returns iterator.
         '''
-        if isinstance( word, genericWord ):
-            return word in self._words
-        elif isinstance( word, str ):
-            return word in [str( word ) for word in self]
+
+        return iter( self._words )
 
 
     def __len__( self ):
@@ -67,25 +47,11 @@ class genericSentence:
         return len( self._words )
 
 
-    def __iter__( self ):
-        '''
-        Returns iterator from list.
-        '''
-
-        return iter( self._words )
-
-
-class readableSentence( genericSentence ):
-    '''
-    Class that represents read sentence.
-    '''
-
-
     def __repr__( self ):
-        return '<readableSentence WORDS={self._words!r}, TAG={self.tag!r}>'.format( self = self )
+        return '<readableSentence WORDS={self._words!r}>'.format( self = self )
 
 
-class writeableSentence( genericSentence ):
+class writeableSentence:
     '''
     Class that represents sentence to write.
     '''
@@ -99,6 +65,35 @@ class writeableSentence( genericSentence ):
         self._words = [cmd_word]
 
 
-    def __repr__( self ):
-        return '<writeSentence WORDS={self._words!r}, TAG={self.tag!r}>'.format( self = self )
+    def __iadd__( self, word ):
+        '''
+        Method to add word object to sentence.
+        sentence += word
+        '''
 
+        if isinstance( word, ApiAttrWord ):
+            raise ApiError( 'can not add more than one tag to sentence' )
+
+        self._words.append( word )
+        return self
+
+
+    def __iter__( self ):
+        '''
+        Returns iterator.
+        As first word command word is set.
+        '''
+
+        return iter( self._words )
+
+
+    def __len__( self ):
+        '''
+        Return length of sentence. How many word objects in sentence.
+        '''
+
+        return len( self._words )
+
+
+    def __repr__( self ):
+        return '<writeableSentence WORDS={self._words!r}>'.format( self = self )
