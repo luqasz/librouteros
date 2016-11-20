322d4078dc42ffc37659149e2541bcc4f2fd090b
Łukasz Kostka <lukasz.kostka@netng.pl>
2016-11-20 23:11:06 +0100

    Library rewrite

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
+[![Build Status](https://travis-ci.org/luqasz/librouteros.svg)](https://travis-ci.org/luqasz/librouteros)
+
 ### About
-Python3 implementation of [routeros api](http://wiki.mikrotik.com/wiki/API).
-
-### Requirements
-* python3.2
-* nosetests3 (for unit testing only)
-* mock (for unit testing only)
-
-### Documentation
-Documentation is hosted as a separate [git repository](https://github.com/uqasz/librouteros-docs).
-It is build automaticly upon commit and generated [here](http://librouteros.readthedocs.org/en/).
+Python2/3 implementation of [routeros api](http://wiki.mikrotik.com/wiki/API).
+This library uses [semantic versioning](http://semver.org/). On major version things may break, so pin version in dependencies.
 
 ### Contributing
-To submit a feature requests or a bug report, please use issues from within github. If you would like to submit a patch please contact author or use pull request. Note however all new features must have unit tests and documentation.
-
-
+To submit a feature requests or a bug report, please use issues from within github. If you would like to submit a patch please contact author or use pull request.
--- librouteros/api.py
+++ librouteros/api.py
@@ -1,83 +1,133 @@
 # -*- coding: UTF-8 -*-
 
+from posixpath import join as pjoin
 
-from librouteros.datastructures import mksnt, parsresp, trapCheck, raiseIfFatal
-from librouteros.exc import LibError
-
-
-class Api:
+from librouteros.exceptions import TrapError, MultiTrapError
 
 
-    def __init__( self, rwo ):
-        self.rwo = rwo
+class Parser:
 
+    api_mapping = {'yes': True, 'true': True, 'no': False, 'false': False}
 
-    def _set_timeout(self, value):
-        if value <= 0:
-            raise ValueError('timeout must be greater than 0')
-        else:
-            self.rwo.sock.settimeout(value)
-
-    def _get_timeout(self):
-        return self.rwo.sock.gettimeout()
-
-
-    timeout = property( _get_timeout, _set_timeout, doc='Get or set timeout of connection. Timeout muste be > 0.' )
-
-
-    def run( self, cmd, args = dict() ):
+    @staticmethod
+    def apiCast(value):
         '''
-        Run any 'non interactive' command. Returns parsed response.
+        Cast value from API to python.
 
-        cmd Command word eg. /ip/address/print.
-        args Dictionary with key, value pairs.
+        :returns: Python equivalent.
         '''
-
-        snt = (cmd,) + mksnt( args )
-        self.rwo.writeSnt( snt )
-        response = self._readDone()
-        trapCheck( response )
-        raiseIfFatal( response )
-
-        return parsresp( response )
-
-
-    def _readDone( self ):
-        '''
-        Read as long as !done is received.
-
-        returns Read sentences as tuple.
-        '''
-
-        snts = []
-
-        while True:
-
-            snt = self.rwo.readSnt()
-            snts.append( snt )
-
-            if '!done' in snt:
-                break
-
-        return tuple( snts )
-
-
-    def close( self ):
-        '''
-        Send /quit and close the connection.
-        '''
-
         try:
-            self.rwo.writeSnt( ('/quit',) )
-            self.rwo.readSnt()
-        except LibError:
-            pass
-        finally:
-            self.rwo.close()
+            casted = int(value)
+        except ValueError:
+            casted = Parser.api_mapping.get(value, value)
+        return casted
+
+    @staticmethod
+    def parseWord(word):
+        '''
+        Split given attribute word to key, value pair.
+        Values are casted to python equivalents.
+
+        :param word: API word.
+        :returns: Key, value pair.
+        '''
+        _, key, value = word.split('=', 2)
+        value = Parser.apiCast(value)
+        return (key, value)
 
 
-    def __del__( self ):
+class Composer:
+
+    python_mapping = {True: 'yes', False: 'no'}
+
+    @staticmethod
+    def pythonCast(value):
         '''
-        On garbage collection run close().
+        Cast value from python to API.
+
+        :returns: Casted to API equivalent.
         '''
-        self.close()
+
+        # this is necesary because 1 == True, 0 == False
+        if type(value) == int:
+            return str(value)
+        else:
+            return Composer.python_mapping.get(value, str(value))
+
+    @staticmethod
+    def composeWord(key, value):
+        '''
+        Create a attribute word from key, value pair.
+        Values are casted to api equivalents.
+        '''
+        return '={}={}'.format(key, Composer.pythonCast(value))
+
+
+class Api(Composer, Parser):
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+
+    def __call__(self, cmd, **kwargs):
+        '''
+        Call Api with given command.
+
+        :param cmd: Command word. eg. /ip/address/print
+        :param kwargs: Dictionary with optional arguments.
+        '''
+        words = tuple(self.composeWord(key, value) for key, value in kwargs.items())
+        self.protocol.writeSentence(cmd, *words)
+        return self._readResponse()
+
+    def _readSentence(self):
+        '''
+        Read one sentence and parse words.
+
+        :returns: Reply word, dict with attribute words.
+        '''
+        reply_word, words = self.protocol.readSentence()
+        words = dict(self.parseWord(word) for word in words)
+        return reply_word, words
+
+    def _readResponse(self):
+        '''
+        Read untill !done is received.
+
+        :throws TrapError: If one !trap is received.
+        :throws MultiTrapError: If > 1 !trap is received.
+        :returns: Full response
+        '''
+        response = []
+        reply_word = None
+        while reply_word != '!done':
+            reply_word, words = self._readSentence()
+            response.append((reply_word, words))
+
+        self._trapCheck(response)
+        # Remove empty sentences
+        return tuple(words for reply_word, words in response if words)
+
+    def close(self):
+        self.protocol.close()
+
+    @staticmethod
+    def _trapCheck(response):
+        traps = tuple(words for reply_word, words in response if reply_word == '!trap')
+        if len(traps) > 1:
+            traps = tuple(
+                    TrapError(message=trap['message'], category=trap.get('category'))
+                    for trap in traps
+                    )
+            raise MultiTrapError(*traps)
+        elif len(traps) == 1:
+            trap = traps[0]
+            raise TrapError(message=trap['message'], category=trap.get('category'))
+
+    @staticmethod
+    def joinPath(*path):
+        '''
+        Join two or more paths forming a command word.
+        >>> api.joinPath('/ip', 'address', 'print')
+        >>> '/ip/address/print'
+        '''
+        return pjoin('/', *path).rstrip('/')
