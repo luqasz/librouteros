5701cc7658e2c0c09aec4d4d8f6d314b050ce207
Łukasz Kostka <lukasz.kostka@netng.pl>
2017-03-01 22:18:13 +0100

    Download images with wget

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,59 +1,8 @@
 import pytest
 import os
-import hashlib
-import requests
 from time import sleep
-from collections import namedtuple
 from subprocess import Popen, PIPE, check_call
 from librouteros import connect, ConnectionError
-
-
-RouterosImage = namedtuple('RouterosImage', ('version', 'sha256', 'id'))
-
-routeros_images = (
-    RouterosImage(
-        version='6.33.3',
-        sha256='be3b9d982d1ffc893ad50d5295a5f4e55f222eb6088cd52ba8d453a6da800a28',
-        id='0B7lhzJhvsd-8Z1IyRVFmSVpHcXc',
-    ),
-)
-
-
-def download_file(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-
-def exists(path, sha256):
-    if os.path.exists(path):
-        with open(path, 'rb') as f:
-            return hashlib.sha256(f.read()).hexdigest() == sha256
-    else:
-        return False
 
 
 def get_api():
@@ -64,13 +13,10 @@ def get_api():
             sleep(1)
 
 
-@pytest.fixture(scope='session', params=routeros_images, ids=lambda v: v.version)
+@pytest.fixture(scope='session', params=('6.33.3',))
 def ros_img(request):
-    dst = 'images/routeros_{}.qcow2'.format(request.param.version)
-    dst = os.path.join(os.getcwd(), dst)
-    if not exists(dst, request.param.sha256):
-        download_file(request.param.id, dst)
-    return dst
+    """Return absolute path to backing disk image."""
+    return os.path.join(os.getcwd(), 'images/routeros_{}.qcow2'.format(request.param))
 
 
 @pytest.fixture(scope='session')
