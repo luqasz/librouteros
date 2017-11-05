1106f13be9a4fdc26339e88c2b196489f18d383c
Łukasz Kostka <lukasz.kostka@netng.pl>
2017-11-05 11:40:22 +0100

    Drop connect() function
    refers to #16

--- librouteros/__init__.py
+++ librouteros/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-from socket import create_connection, error as SOCKET_ERROR, timeout as SOCKET_TIMEOUT
 from binascii import unhexlify, hexlify
 from hashlib import md5
 try:
@@ -14,30 +13,24 @@ from librouteros.api import Api
 
 
 defaults = {
-            'timeout': 10,
-            'port': 8728,
-            'saddr': '',
             'subclass': Api,
             'encoding': 'ASCII',
             }
 
 
-def connect(host, username, password, **kwargs):
+def login(username, password, sock, **kwargs):
     """
-    Connect and login to routeros device.
+    Login to routeros device.
 
     Upon success return a Connection class.
 
-    :param host: Hostname to connecto to. May be ipv4,ipv6,FQDN.
     :param username: Username to login with.
     :param password: Password to login with. Only ASCII characters allowed.
-    :param timeout: Socket timeout. Defaults to 10.
-    :param port: Destination port to be used. Defaults to 8728.
-    :param saddr: Source address to bind to.
+    :param sock: Connected socket. May be SSL/TLS or plain TCP.
     :param subclass: Subclass of Api class. Defaults to Api class from library.
     """
     arguments = ChainMap(kwargs, defaults)
-    transport = create_transport(host, **arguments)
+    transport = SocketTransport(sock=sock)
     protocol = ApiProtocol(transport=transport, encoding=arguments['encoding'])
     api = arguments['subclass'](protocol=protocol)
 
@@ -45,20 +38,12 @@ def connect(host, username, password, **kwargs):
         sentence = api('/login')
         token = sentence[0]['ret']
         encoded = encode_password(token, password)
-        api('/login', **{'name': username, 'response': encoded})
+        api('/login', name=username, response=encoded)
     except (ConnectionError, TrapError, FatalError, MultiTrapError):
         transport.close()
         raise
 
     return api
-
-
-def create_transport(host, **kwargs):
-    try:
-        sock = create_connection((host, kwargs['port']), kwargs['timeout'], (kwargs['saddr'], 0))
-    except (SOCKET_ERROR, SOCKET_TIMEOUT) as error:
-        raise ConnectionError(error)
-    return SocketTransport(sock=sock)
 
 
 def encode_password(token, password):
