7f1d84e835c49221fb101c42168d5ce456f3f984
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-08-19 01:15:27 +0200

    Fix pip 18.x installation

---
+++ tests/requirements/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from subprocess import check_output
+import sys
+
+
+@pytest.fixture
+def installed_packages():
+    pkgs = []
+    pip_executable = [sys.executable, '-m', 'pip']
+    result = check_output(pip_executable + ["freeze", "--local", "--all"])
+    for line in result.decode().splitlines():
+        if '==' in line:
+            pkg, version = line.split('==', 1)
+            pkgs.append(pkg)
+
+    return pkgs
