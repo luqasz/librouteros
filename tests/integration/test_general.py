d4d6832fa6d18b011fb6a4de70f3c015069ebb7f
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-03-04 21:47:32 +0100

    Consume generator in tests

--- tests/integration/test_general.py
+++ tests/integration/test_general.py
@@ -1,6 +1,6 @@
 def test_hostname(routeros):
     data = routeros('/system/identity/print')
-    assert data[0]['name'] == 'MikroTik'
+    assert tuple(data)[0]['name'] == 'MikroTik'
 
 
 def test_long_word(routeros):
@@ -13,12 +13,13 @@ def test_long_word(routeros):
     Check if when reading back, comment is same when set.
     """
     long_value = 'a' * (256 - len('=comment='))
-    ID = routeros(
+    data = routeros(
             '/ip/address/add',
             address='172.16.1.1/24',
             interface='ether1',
             comment=long_value
-        )[0]['ret']
+        )
+    ID = tuple(data)[0]['ret']
     for row in routeros('/ip/address/print'):
         if row['.id'] == ID:
             assert row['comment'] == long_value
