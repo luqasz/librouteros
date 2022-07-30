2bc47581fee0c3ac525c9161b6ddecce2c287a75
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-07-31 00:19:52 +0200

    Remove redundant tests

--- tests/unit/test_connections.py
+++ tests/unit/test_connections.py
@@ -17,10 +17,6 @@ class Test_SocketTransport:
     def setup(self):
         self.transport = SocketTransport(sock=MagicMock(spec=socket))
 
-    def test_calls_socket_close(self):
-        self.transport.close()
-        self.transport.sock.close.assert_called_once()
-
     def test_close_shutdown_exception(self):
         self.transport.sock.shutdown.side_effect = SOCKET_ERROR
         self.transport.close()
@@ -34,12 +30,6 @@ class Test_SocketTransport:
         self.transport.write(b'some message')
         self.transport.sock.sendall.assert_called_once_with(b'some message')
 
-    @pytest.mark.parametrize("exception", (SOCKET_ERROR, SOCKET_TIMEOUT))
-    def test_write_raises_socket_errors(self, exception):
-        self.transport.sock.sendall.side_effect = exception
-        with pytest.raises(exception):
-            self.transport.write(b'some data')
-
     def test_read_raises_when_recv_returns_empty_byte_string(self):
         self.transport.sock.recv.return_value = b''
         with pytest.raises(ConnectionClosed):
@@ -52,12 +42,6 @@ class Test_SocketTransport:
         """
         self.transport.sock.recv.side_effect = (b'retu', b'rne', b'd')
         assert self.transport.read(8) == b'returned'
-        # Check if we ask only for what is left after each recv()
-        assert self.transport.sock.recv.call_args_list == [
-            call(8),
-            call(4),
-            call(1),
-        ]
 
     @pytest.mark.parametrize("exception", (SOCKET_ERROR, SOCKET_TIMEOUT))
     def test_recv_raises_socket_errors(self, exception):
