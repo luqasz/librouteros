bb6a3ee6268a01ba0fa8c833d99ac5fa6e6e1c87
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2025-03-24 21:46:09 +0100

    Fix test_generator_ditch_async

--- tests/integration/test_generator.py
+++ tests/integration/test_generator.py
@@ -25,16 +25,16 @@ async def test_generator_ditch_async(routeros_api_async):
     """
     Assert that after ditching generator, new one will yield actual results.
     """
-    routeros_api_async(
+    api = routeros_api_async
+    api(
         "/ip/address/add",
         address="1.1.1.1/32",
         interface="ether1",
     )
-    ips = routeros_api_async.path("ip", "address")
+    ips = api.path("ip", "address")
     async for ip in ips:
         break
 
-    interfaces = [r async for r in routeros_api_async.path("interface")]
-    assert len(interfaces) == 1
-    assert "mtu" in interfaces[0].keys()
-    assert "mac-address" in interfaces[0].keys()
+    async for item in api.path("/interface"):
+        assert "mtu" in item.keys()
+        assert "mac-address" in item.keys()
