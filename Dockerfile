7f57a6e2380b00a920b119dae4c98a68a96a6682
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2025-08-01 23:39:37 +0200

    Update dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+
+COPY images/*.qcow2 /opt/
--- docker/do.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for ver in 3.8 3.9 3.10 3.11 3.12
-do
-  docker buildx build --platform linux/amd64,linux/arm64 --push -t lukaszkostka/librouteros:${ver} -f docker/${ver}.dockerfile .
-done
