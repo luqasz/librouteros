825eb73d9af9bc4e8d79e870b961aa46f059e51d
Alejandro Liu <alejandro_liu@hotmail.com>
2025-12-18 16:45:15 +0100

    Adding support for proxy_command

--- src/librouteros/__init__.py
+++ src/librouteros/__init__.py
@@ -2,7 +2,13 @@
 
 import asyncio
 from collections import ChainMap
+import os
+import re
+import shlex
+import signal
+import socket
 from socket import create_connection
+import subprocess
 
 from librouteros.api import Api, AsyncApi
 from librouteros.connections import AsyncSocketTransport, SocketTransport
@@ -26,6 +32,8 @@ DEFAULTS = {
     "encoding": "ASCII",
     "ssl_wrapper": None,
     "login_method": plain,
+    'proxy_command': None,
+    'ignore_intr': False,
 }
 
 ASYNC_DEFAULTS = {
@@ -36,6 +44,8 @@ ASYNC_DEFAULTS = {
     "encoding": "ASCII",
     "ssl_wrapper": None,
     "login_method": async_plain,
+    'proxy_command': None,
+    'ignore_intr': False,
 }
 
 
@@ -53,6 +63,8 @@ def connect(host: str, username: str, password: str, **kwargs) -> Api:
     :param subclass: Subclass of Api class. Defaults to Api class from library.
     :param ssl_wrapper: Callable (e.g. ssl.SSLContext instance) to wrap socket with.
     :param login_method: Callable with login method.
+    :param proxy_command: String used for proxyed connections.  Understands %h and %p similar to openssh proxy_command option.
+    :param ignore_intr: Make proxy_command ignore INTR signals
     """
     arguments = ChainMap(kwargs, DEFAULTS)
     transport = create_transport(host, **arguments)
@@ -81,6 +93,8 @@ async def async_connect(host: str, username: str, password: str, **kwargs) -> As
     :param subclass: Subclass of Api class. Defaults to Api class from library.
     :param ssl_wrapper: Callable (e.g. ssl.SSLContext instance) to wrap socket with.
     :param login_method: Callable with login method.
+    :param proxy_command: String used for proxyed connections.  Understands %h and %p similar to openssh proxy_command option.
+    :param ignore_intr: Make proxy_command ignore INTR signals
     """
     arguments = ChainMap(kwargs, ASYNC_DEFAULTS)
     transport = await async_create_transport(host, **arguments)
@@ -95,25 +109,83 @@ async def async_connect(host: str, username: str, password: str, **kwargs) -> As
         raise
 
 
+def proxy_connect(hostport:tuple[str,int], proxy_cmd:str, ignore_intr:bool = False) -> socket.socket:
+    host,port = hostport
+    mapping = {'%h': host, '%p': str(port) }
+    cmdline = shlex.split(re.sub(r'%[hp]',
+                    lambda m: mapping[m.group(0)], proxy_cmd))
+    s1, s2 = socket.socketpair()
+
+    if os.name == 'posix':
+        # This is very Linux oriented, and done at a fairly low level
+        # Should have less overhead.
+        pid = os.fork()
+        if pid == 0:
+            # Child runs the proxy command
+            s2.close()
+            fd = s1.fileno()
+            os.dup2(fd, 0)
+            os.dup2(fd, 1)
+            if ignore_intr: signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+            os.execvp(cmdline[0], cmdline)
+            exit(-1)  # Abort if we reach here!
+    else:
+        # This more portable version should work on Windows
+        ic(cmdline)
+        s1_in = s1.makefile('rb', buffering=0)
+        s1_out = s1.makefile('wb', buffering=0)
+        proc = subprocess.Popen(
+                cmdline,
+                stdin=s1_in,
+                stdout=s1_out,
+                stderr=subprocess.DEVNULL,
+                creationflags=subprocess.CREATE_NO_WINDOW if ignore_intr else 0
+            )
+
+    # Close our copies of s1; keep s2 for communication
+    s1.close()
+    return s2
+
 def create_transport(host: str, **kwargs) -> SocketTransport:
-    sock = create_connection(
-        (host, kwargs["port"]),
-        timeout=kwargs["timeout"],
-        source_address=(kwargs["saddr"], 0),
-    )
+    if kwargs['proxy_command'] is None:
+        sock = create_connection(
+            (host, kwargs["port"]),
+            timeout=kwargs["timeout"],
+            source_address=(kwargs["saddr"], 0),
+        )
+    else:
+        sock = proxy_connect(
+                (host, kwargs["port"]),
+                kwargs['proxy_command'],
+                kwargs['ignore_intr'],
+        )
     if wrapper := kwargs["ssl_wrapper"]:
         sock = wrapper(sock)
     return SocketTransport(sock=sock)
 
 
 async def async_create_transport(host: str, **kwargs) -> AsyncSocketTransport:
-    reader, writer = await asyncio.wait_for(
-        asyncio.open_connection(
-            host=host,
-            port=kwargs["port"],
-            ssl=kwargs["ssl_wrapper"],
-            local_addr=(kwargs["saddr"], 0),
-        ),
-        timeout=kwargs["timeout"],
-    )
+    if kwargs['proxy_command'] is None:
+        reader, writer = await asyncio.wait_for(
+          asyncio.open_connection(
+              host=host,
+              port=kwargs["port"],
+              ssl=kwargs["ssl_wrapper"],
+              local_addr=(kwargs["saddr"], 0),
+          ),
+          timeout=kwargs["timeout"],
+        )
+    else:
+        sock = proxy_connect(
+                (host, kwargs["port"]),
+                kwargs['proxy_command'],
+                kwargs['ignore_intr'],
+        )
+        # Wrap the socket in the asyncio loop...
+        reader, writer = await asyncio.wait_for(
+          asyncio.open_connection(sock = sock),
+          timeout=kwargs["timeout"],
+        )
+
     return AsyncSocketTransport(reader=reader, writer=writer)
